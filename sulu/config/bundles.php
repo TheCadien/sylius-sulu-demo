a71df33438e04318185a716128ac6dd0b91906f9
Oliver Kossin <thecadiens@gmail.com>
2021-04-10 11:16:36 +0200

    init maker bundle

--- sulu/config/bundles.php
+++ sulu/config/bundles.php
@@ -43,4 +43,5 @@ return [
     Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle::class => ['all' => true, 'website' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Sulu\Bundle\HeadlessBundle\SuluHeadlessBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
