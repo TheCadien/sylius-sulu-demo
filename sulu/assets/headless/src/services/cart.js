814f8c647027052b33c19b0281d4beab6fb8b626
Johannes Wachter <johannes@sulu.io>
2021-04-29 07:37:50 +0200

    added composer lock to sylius

--- sulu/assets/headless/src/services/cart.js
+++ sulu/assets/headless/src/services/cart.js
@@ -1,10 +1,24 @@
+import shopRequester from './shopRequester';
+
 const createCart = () => {
+    return shopRequester.post('/api/v2/shop/orders', { locale: 'en_US' });
 };
 
 const addItemToCart = (cart, code, variantCode, quantity) => {
+    return shopRequester.patch('/api/v2/shop/orders/' + cart.tokenValue + '/items', {
+        productCode: code,
+        productVariantCode: variantCode,
+        quantity: quantity,
+    });
 };
 
+const loadProduct = (code) => {
+    shopRequester.get('/api/v2/shop/products/' + code).then((data) => {
+        return shopRequester.get(data.variants[0])
+    });
+};
 export {
     createCart,
     addItemToCart,
+    loadProduct,
 };
